ea10c7f8647c884cae3cbf0429c5f76efc0df69a
Pascal Muskalla <muskalla.pasca-it23@it.dhbw-ravensburg.de>
2025-11-21 13:19:03 +0100

    Übung 3 commiten

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,6 +1,21 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
+int status = 0;
+bool direc = false; // false = hoch    true = runter
+bool user_button = false;   // fehrnbedienung
+bool endlage_oben = false;  
+bool endlage_unten = false;
+bool motor_an = false;      // Motor aktiv schalten
+
+int Endlage(int stat){
+    if (endlage_oben == true){
+        fmt::print("Endlage oben erreicht")
+        stat = true;
+
+    }
+}
+
 auto main(int argc, char** argv) -> int
 {
     /**
@@ -10,6 +25,30 @@ auto main(int argc, char** argv) -> int
      */
     fmt::print("Hello, {}!\n", argv[0]);
 
+    while(true){
 
+        switch(status){
+            case 0:
+                fmt::print("Stop");
+                motor_an = false;
+
+                break;
+            
+            case 1: 
+                fmt::print("Fahre hoch");
+                direc = 0;
+                motor_an = true;
+
+                break;
+            case 2:
+                fmt::print("Fahre runter");
+                direc = 1;
+                motor_an = true;
+
+                break;
+        }
+    }
     return 0; /* exit gracefully*/
 }
+    
+
